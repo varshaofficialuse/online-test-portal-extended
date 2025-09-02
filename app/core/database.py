3af409efcd4f92fe71a8f61201e4a0caa6b5e9b4
varshaofficialuse <varshaofficialuse@gmail.com>
2025-09-02 19:04:58 +0530

    db string print comment removed

--- app/core/database.py
+++ app/core/database.py
@@ -6,7 +6,7 @@ from app.core.config import settings
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True,pool_size=10, max_overflow=5, future=True, pool_timeout=30)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-print('------------',settings.DATABASE_URL)
+
 
 def get_db():
     db = SessionLocal()
